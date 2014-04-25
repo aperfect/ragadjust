95c06d9073a31231f545807344fc8ca38d29d9ad
Adam Perfect <aperfect@gmail.com>
2014-04-25 15:23:52 +0100

    Almost working, but &nbsp; getting escaped

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Ragadjust
 
-TODO: Write a gem description
+Gem to rag-adjust text content based on Mark Boulton's 24ways article: http://24ways.org/2013/run-ragged/ and Nathan Ford's JS implementation: https://github.com/nathanford/ragadjust
 
 ## Installation
 
@@ -22,7 +22,7 @@ TODO: Write usage instructions here
 
 ## Contributing
 
-1. Fork it ( https://github.com/[my-github-username]/ragadjust/fork )
+1. Fork it ( https://github.com/aperfect/ragadjust/fork )
 2. Create your feature branch (`git checkout -b my-new-feature`)
 3. Commit your changes (`git commit -am 'Add some feature'`)
 4. Push to the branch (`git push origin my-new-feature`)
--- lib/ragadjust/adjust.rb
+++ lib/ragadjust/adjust.rb
@@ -9,64 +9,77 @@ module Ragadjust
     def self.ragadjust_content(text_to_adjust, selector = 'p, li, dd, figcaption', method = 'all')
       html = Nokogiri::HTML(text_to_adjust) # Not using .fragment() as .search() doesn't seem to work?
 
-      elements = html.css(selector)
-
       preps = /(?<=\s|^|;)((aboard|about|above|across|after|against|along|amid|among|anti|around|before|behind|below|beneath|beside|besides|between|beyond|concerning|considering|despite|down|during|except|excepting|excluding|following|from|inside|into|like|minus|near|onto|opposite|outside|over|past|plus|regarding|round|save|since|than|that|this|through|toward|towards|under|underneath|unlike|until|upon|versus|with|within|without)\s)+/i
           
       smallwords = /(?<=\s|^|;)\b(([[:word:]]{1,2})\b\s)/i
       
       dashes = /([-–—])\s/i
 
+    
+      elements = html.css(selector)
 
-      # prepositions
-      if %w{prepositions all}.include? method
-        # Get text nodes
-        elements.search('//text()').each do |t|
-          text_node_replace t, preps, '\2&nbsp;'
-        end
-      end
+      elements.search('//text()').each do |node|
 
-      # small words
-      if %w{smallwords all}.include? method
-        # Get text nodes
-        elements.search('//text()').each do |t|
-          text_node_replace t, smallwords, '\2&nbsp;'
-        end
-      end
+        if node.text?
+          content = node.content
 
-      # dashes
-      if %w{dashes all}.include? method
-        # Get text nodes
-        elements.search('//text()').each do |t|
-          text_node_replace t, dashes, '\1&nbsp;'
+          puts "Got content: #{content}"
+          puts "Got parent: #{node.parent}"
+          puts "Got parent inner_html: #{node.parent.inner_html}"
+
+          # prepositions
+          if %w{prepositions all}.include? method
+            content = content.gsub preps, '\2&nbsp;'
+            puts "Content after preps: #{content}"
+          end
+
+          # small words
+          if %w{small-words all}.include? method
+            content = content.gsub smallwords, '\2&nbsp;'
+            puts "Content after small words: #{content}"
+          end
+
+          # dashes
+          if %w{dashes all}.include? method
+            content = content.gsub dashes, '\1&nbsp;'
+            puts "Content after dashes: #{content}"
+          end
+
+          # node.content = ''
+          # puts "Setting inner_html to: #{content}"
+          # node.parent.inner_html = content
+          # puts "Node parent inner_html = #{node.parent.inner_html}"
+          dummy = node.add_previous_sibling(Nokogiri::XML::Node.new('dummy', html))
+          new_node = Nokogiri::XML::Text.new('', html)
+          new_node.inner_html = content
+          
+          dummy.add_previous_sibling(new_node)
+          node.remove
+          dummy.remove
         end
+
       end
 
       # emphasized text
-      if %w{prepositions all}.include? method
-        elements.css('strong,em,b,i').each do |el|
-          # Only emphasized phrases of 2/3 words
-          s = el.content.split.size
-          if s > 1 && s < 4
-            # Get text nodes
-            el.search('//text()').each do |n|
-              if n.text?
-                text_node_replace n, /\s+/, '&nbsp;'
-              end
-            end
-          end
-        end # elements.css().each
-      end
+      # if %w{emphasis all}.include? method
+      #   puts "Checking emphasised content..."
+      #   elements.css('strong,em,b,i').each do |el|
+      #     # Only emphasized phrases of 2/3 words
+      #     s = el.content.split.size
+      #     if s > 1 && s < 4
+      #       # Get text nodes
+      #       el.search('//text()').each do |n|
+      #         if n.text?
+      #           Ragadjust::Adjust.text_node_replace n, /\s+/, '&nbsp;'
+      #         end
+      #       end
+      #     end
+      #   end # elements.css().each
+      # end
 
       html.css('body').inner_html
 
     end # def self.ragadjust_content
-
-    def self.text_node_replace(node, pattern, replacement)
-      content = node.content.gsub(pattern, replacement)
-      node.content = ''
-      node.inner_html = content
-    end
 
   end
 
