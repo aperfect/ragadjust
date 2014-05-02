f7c8aa0e9273cae5aa7a0bc615a4d36af99d5ba0
Adam Perfect <aperfect@gmail.com>
2014-05-01 19:47:10 -0700

    Emphasised text back in

--- lib/ragadjust/adjust.rb
+++ lib/ragadjust/adjust.rb
@@ -23,62 +23,41 @@ module Ragadjust
         if node.text?
           content = node.content
 
-          puts "Got content: #{content}"
-          puts "Got parent: #{node.parent}"
-          puts "Got parent inner_html: #{node.parent.inner_html}"
-
           # prepositions
           if %w{prepositions all}.include? method
-            # content = content.gsub preps, '\2&nbsp;'
             content = content.gsub preps, '\2'+nbsp
-            puts "Content after preps: #{content}"
           end
 
           # small words
           if %w{small-words all}.include? method
-            # content = content.gsub smallwords, '\2&nbsp;'
             content = content.gsub smallwords, '\2'+nbsp
-            puts "Content after small words: #{content}"
           end
 
           # dashes
           if %w{dashes all}.include? method
-            # content = content.gsub dashes, '\1&nbsp;'
             content = content.gsub dashes, '\1'+nbsp
-            puts "Content after dashes: #{content}"
           end
 
           node.content = content
-          # puts "Setting inner_html to: #{content}"
-          # node.parent.inner_html = content
-          # puts "Node parent inner_html = #{node.parent.inner_html}"
-          # dummy = node.add_previous_sibling(Nokogiri::XML::Node.new('dummy', html))
-          # new_node = Nokogiri::XML::Text.new('', html)
-          # new_node.inner_html = content
-
-          # dummy.add_previous_sibling(new_node)
-          # node.remove
-          # dummy.remove
         end
 
       end
 
       # emphasized text
-      # if %w{emphasis all}.include? method
-      #   puts "Checking emphasised content..."
-      #   elements.css('strong,em,b,i').each do |el|
-      #     # Only emphasized phrases of 2/3 words
-      #     s = el.content.split.size
-      #     if s > 1 && s < 4
-      #       # Get text nodes
-      #       el.search('//text()').each do |n|
-      #         if n.text?
-      #           Ragadjust::Adjust.text_node_replace n, /\s+/, '&nbsp;'
-      #         end
-      #       end
-      #     end
-      #   end # elements.css().each
-      # end
+      if %w{emphasis all}.include? method
+        elements.css('strong,em,b,i').each do |el|
+          # Only emphasized phrases of 2/3 words
+          s = el.content.split.size
+          if s > 1 && s < 4
+            # Get text nodes
+            el.search('//text()').each do |n|
+              if n.text?
+                Ragadjust::Adjust.text_node_replace n, /\s+/, nbsp
+              end
+            end
+          end
+        end # elements.css().each
+      end
 
       html.css('body').inner_html
 
