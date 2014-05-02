59c5b765a438382a4328c39b4fa9c4fcfb30718b
Adam Perfect <aperfect@gmail.com>
2014-05-01 20:58:13 -0700

    Oops

--- lib/ragadjust/adjust.rb
+++ lib/ragadjust/adjust.rb
@@ -52,7 +52,7 @@ module Ragadjust
             # Get text nodes
             el.search('//text()').each do |n|
               if n.text?
-                n.gsub /\s+/, nbsp
+                n.content = n.content.gsub /\s+/, nbsp
               end
             end
           end
